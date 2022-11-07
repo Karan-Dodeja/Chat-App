fa9f87c7ed7fe80d34c5429fb53e4172c0ab1086
KaranDodeja03 <karandodeja03@gmail.com>
2022-11-08 01:00:02 +0530

    Routing Setup

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import Home from "./pages/Home.jsx";
 import "./style.scss";
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext.js";
 import { useContext } from "react";
 
 function App() {
-  const {currentUser} = useContext(AuthContext);
-  console.log(currentUser);
+  const { currentUser } = useContext(AuthContext);
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+  }
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" >
-          <Route index element={<Home />} />
+          <Route index element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
